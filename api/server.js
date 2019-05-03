4da6b5f987f2aa90311483e926f5b6e12e71192d
Josh Akeman <joshakeman@gmail.com>
2019-05-03 11:18:03 -0400

    Projects and actions routers running

--- api/server.js
+++ api/server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const projectsRouter = require('../routers/projectsRouter')
+const actionsRouter = require('../routers/actionsRouter')
 
 const server = express()
 
 server.use(express.json());
+
+server.use('/api/projects', projectsRouter)
+server.use('/api/actions', actionsRouter)
+
 
 server.get('/', (req, res) => {
     res.status(200).json({message: 'Server is running'})
